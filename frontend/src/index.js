60b42d9d64656651bf54d6b9d3ff4a09678a7a8d
fedechaves <federicochavesf@gmail.com>
2022-12-27 19:26:30 -0300

    feat: added redux, provider, reducers and models

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,16 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+import { Provider }  from 'react-redux';
+import {createStore, applyMiddleware} from 'redux'
+import {rootReducer} from './reducers';
+
+import { createLogger } from 'redux-logger';
+
+const loggerMiddleware = createLogger(); 
+
+const store = createStore(rootReducer, applyMiddleware(loggerMiddleware))
+
+store.dispatch({ type: "MEH" })
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
